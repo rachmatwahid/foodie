ea21ed11293856b73f0d692bc625f373b1d366e7
rachmatwahid <32413014+rachmatwahid@users.noreply.github.com>
2022-07-19 08:00:58 +0700

    Initial commit

--- app/src/main/java/com/rachmatwahid/foodie/MainActivity.java
+++ app/src/main/java/com/rachmatwahid/foodie/MainActivity.java
@@ -3,12 +3,29 @@ package com.rachmatwahid.foodie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private byte stock = 0;
+    private TextView quantityTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        quantityTextView = findViewById(R.id.quantityTextView);
+    }
+
+    public void addOne(View view) {
+        stock ++;
+        quantityTextView.setText(Byte.toString(stock));
+    }
+
+    public void substractOne(View view) {
+        stock --;
+        quantityTextView.setText(Byte.toString(stock));
     }
 }
