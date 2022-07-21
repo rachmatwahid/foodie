f4486b5bb8529bffbc0a7fc4412ce8ab1412ebc5
rachmatwahid <32413014+rachmatwahid@users.noreply.github.com>
2022-07-21 08:10:21 +0700

    Initial commit

---
+++ app/src/main/java/com/rachmatwahid/foodie/DetailActivity.java
@@ -0,0 +1,14 @@
+package com.rachmatwahid.foodie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
