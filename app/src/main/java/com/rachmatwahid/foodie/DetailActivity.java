fc83d36e2e932569be08239dc35bc2f62c5dedae
rachmatwahid <r4ChM4tW4h1D>
2022-07-26 10:44:01 +0700

    Initial commit

--- app/src/main/java/com/rachmatwahid/foodie/DetailActivity.java
+++ app/src/main/java/com/rachmatwahid/foodie/DetailActivity.java
@@ -2,13 +2,33 @@ package com.rachmatwahid.foodie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    TextView foodNameTextView;
+
+    public static final String EXTRA_ORDER = "ORDER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        String foodName = intent.getStringExtra(MainActivity.EXTRA_FOOD_NAME);
+
+        foodNameTextView = findViewById(R.id.foodName_textView);
+        foodNameTextView.setText(foodName);
+    }
+
+    public void orderNow(View view) {
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_ORDER, 1);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
--- app/src/main/java/com/rachmatwahid/foodie/MainActivity.java
+++ app/src/main/java/com/rachmatwahid/foodie/MainActivity.java
@@ -1,15 +1,23 @@
 package com.rachmatwahid.foodie;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private byte stock = 0;
+    private byte quantity = 0;
+    private String foodName = "Fried Rice";
+
     private TextView quantityTextView;
+
+    public static final String EXTRA_FOOD_NAME = "NAME";
+    public static final int ORDER_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +25,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         quantityTextView = findViewById(R.id.quantityTextView);
+
+        if (savedInstanceState != null) {
+            quantity = savedInstanceState.getByte("SAVED_QUANTITY");
+            quantityTextView.setText(Byte.toString(quantity));
+        }
     }
 
     public void addOne(View view) {
-        stock ++;
-        quantityTextView.setText(Byte.toString(stock));
+        quantity++;
+        quantityTextView.setText(Byte.toString(quantity));
     }
 
     public void substractOne(View view) {
-        stock --;
-        quantityTextView.setText(Byte.toString(stock));
+        quantity--;
+        quantityTextView.setText(Byte.toString(quantity));
+    }
+
+    public void launchDetailActivity(View view) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(EXTRA_FOOD_NAME, foodName);
+        startActivityForResult(intent, ORDER_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ORDER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                int orderRequest = data.getIntExtra(DetailActivity.EXTRA_ORDER, 0);
+                quantity += orderRequest;
+                quantityTextView.setText(Byte.toString(quantity));
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putByte("SAVED_QUANTITY", quantity);
     }
 }
